21abe9412b3e4598c9f4796b0b5584fcbd187a72
bsaha <bee.saha@yahoo.com>
2022-09-13 06:50:15 +0530

    seventh commit: 2nd BankAccount Test added

--- test-app/src/test/java/com/saha/maven/TestBankAccount.java
+++ test-app/src/test/java/com/saha/maven/TestBankAccount.java
@@ -12,5 +12,12 @@ public class TestBankAccount {
 		double amt = acct.debit(5);
 		assertEquals(5.0, amt, 0.0001);
 	}
+	
+	@Test
+	public void testDebitWithInSufficientFunds() {
+		BankAccount acct = new BankAccount(10);
+		double amt = acct.debit(11);
+		assertEquals(10.0, amt, 0.0001);
+	}
 
 }
